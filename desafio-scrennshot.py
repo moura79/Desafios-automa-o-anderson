75ddd8176652414f6bdd3a2188519d1af3a92f79
Anderson de Moura <anndersonnmoura@gmail.com>
2024-08-26 18:22:27 -0300

    versão 1 print da tela lugar especifico

---
+++ desafio-scrennshot.py
@@ -0,0 +1,5 @@
+# Crie um orograma que tire um print do bloco de notas
+import pyautogui
+
+# Usando o pyautogui localize a coordenada do bloco de notas
+pyautogui.screenshot('Bloco de notas.jpg',region=(964,22,300,337))
